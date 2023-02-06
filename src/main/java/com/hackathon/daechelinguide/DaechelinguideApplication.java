c83ae6e98f2f80507639506ceffec53272791dac
jiseok419 <pyosik0419@gmail.com>
2023-02-07 08:59:35 +0900

    :tada:	:: 기초 설정

---
+++ src/main/java/com/hackathon/daechelinguide/DaechelinguideApplication.java
@@ -0,0 +1,13 @@
+package com.hackathon.daechelinguide;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DaechelinguideApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DaechelinguideApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hackathon/daechelinguide/DaechelinguideApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hackathon.daechelinguide;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DaechelinguideApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
