bc885cb3bcf9fc5ec2239f939fbfa26ba943bb5b
jiseok419 <pyosik0419@gmail.com>
2023-02-07 17:09:15 +0900

    :sparkles: :: Menu 따로 보기 추가

---
+++ src/main/java/com/hackathon/daechelinguide/domain/menu/presentation/dto/response/BreakfastResponseDto.java
@@ -0,0 +1,10 @@
+package com.hackathon.daechelinguide.domain.menu.presentation.dto.response;
+
+import lombok.*;
+
+@Getter
+@Builder
+@AllArgsConstructor @NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class BreakfastResponseDto {
+    private String breakfast;
+}
