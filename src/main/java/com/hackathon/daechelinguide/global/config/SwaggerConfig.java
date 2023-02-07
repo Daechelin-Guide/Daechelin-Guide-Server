186734a332477949bde0b629e76662bc506a3cae
jiseok419 <pyosik0419@gmail.com>
2023-02-07 12:32:34 +0900

    :heavy_plus_sign: :: swagger 설정

---
+++ src/main/java/com/hackathon/daechelinguide/global/config/SwaggerConfig.java
@@ -0,0 +1,22 @@
+package com.hackathon.daechelinguide.global.config;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springdoc.core.GroupedOpenApi;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+    @Bean
+    public OpenAPI openAPI() {
+
+        Info info = new Info()
+                .version("v1.0.0")
+                .title("API 타이틀")
+                .description("API Description");
+
+        return new OpenAPI()
+                .info(info);
+    }
+}
