a233fc91f8892afc25350e071f2343e4788c0953
jiseok419 <pyosik0419@gmail.com>
2023-02-07 17:07:37 +0900

    :sparkles: :: Menu 쿼리 추가

--- src/main/java/com/hackathon/daechelinguide/domain/menu/domain/repository/MenuRepository.java
+++ src/main/java/com/hackathon/daechelinguide/domain/menu/domain/repository/MenuRepository.java
@@ -4,6 +4,11 @@ import com.hackathon.daechelinguide.domain.menu.Menu;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface MenuRepository extends JpaRepository<Menu, Long> {
+    boolean existsByDate(String date);
+
+    Optional<Menu> findByDate(String date);
 }
