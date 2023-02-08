a877fb95d9506c5e81b9fcdca86f0e6d641b0fe0
jiseok419 <pyosik0419@gmail.com>
2023-02-08 12:33:22 +0900

    :sparkles: :: 댓글 쿼리문 추가

--- src/main/java/com/hackathon/daechelinguide/domain/comment/domain/repository/CommentRepository.java
+++ src/main/java/com/hackathon/daechelinguide/domain/comment/domain/repository/CommentRepository.java
@@ -2,9 +2,15 @@ package com.hackathon.daechelinguide.domain.comment.domain.repository;
 
 import com.hackathon.daechelinguide.domain.comment.Comment;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface CommentRepository extends JpaRepository<Comment, Long> {
-
+    @Query(value = "select * from comment where date = :dates AND menu = :menus order by comment_id DESC ",nativeQuery = true)
+    Optional<List<Comment>> findByMessage(@Param("menus") String menus, @Param("dates") String dates);
 }
