eee25de3abc181d9ac12fd424801d9e834e05feb
jiseok419 <pyosik0419@gmail.com>
2023-02-08 12:31:28 +0900

    :sparkles: :: 비즈니스 로직 추가

--- src/main/java/com/hackathon/daechelinguide/domain/comment/presentation/CommentController.java
+++ src/main/java/com/hackathon/daechelinguide/domain/comment/presentation/CommentController.java
@@ -1,4 +1,26 @@
 package com.hackathon.daechelinguide.domain.comment.presentation;
 
+import com.hackathon.daechelinguide.domain.comment.Comment;
+import com.hackathon.daechelinguide.domain.comment.presentation.dto.request.CommentRequestDto;
+import com.hackathon.daechelinguide.domain.comment.service.CommentService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/comment")
+@RequiredArgsConstructor
 public class CommentController {
+    private final CommentService commentService;
+
+    @PostMapping("/regis")
+    public Comment register(@RequestBody CommentRequestDto commentRequestDto){
+        return commentService.register(commentRequestDto);
+    }
+
+    @GetMapping("/message")
+    public List<Comment> findMessage(@RequestParam String menu){
+        return commentService.findComment(menu);
+    }
 }
--- src/main/java/com/hackathon/daechelinguide/domain/comment/service/CommentService.java
+++ src/main/java/com/hackathon/daechelinguide/domain/comment/service/CommentService.java
@@ -1,15 +1,37 @@
 package com.hackathon.daechelinguide.domain.comment.service;
 
+import com.hackathon.daechelinguide.domain.comment.Comment;
 import com.hackathon.daechelinguide.domain.comment.domain.repository.CommentRepository;
+import com.hackathon.daechelinguide.domain.comment.presentation.dto.request.CommentRequestDto;
 import com.hackathon.daechelinguide.domain.menu.domain.repository.MenuRepository;
+import com.hackathon.daechelinguide.global.exception.BusinessException;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDate;
+import java.util.List;
+@Slf4j
 @Service
 @RequiredArgsConstructor
 public class CommentService {
     private final CommentRepository commentRepository;
     private final MenuRepository menuRepository;
 
-    public
+    public Comment register(CommentRequestDto commentRequestDto){
+        return commentRepository.save(Comment.builder()
+                .createdDate(LocalDate.now())
+                .message(commentRequestDto.getMessage())
+                .date(menuRepository.findByDates(commentRequestDto.getMenu()))
+                .menu(commentRequestDto.getMenu())
+                .build());
+    }
+
+    public List<Comment> findComment(String menu){
+        String date = menuRepository.findByDates(menu);
+        return commentRepository.findByMessage(menu, date).orElseThrow(()
+                -> new BusinessException(HttpStatus.NOT_FOUND, "댓글이 존재하지 않습니다.")
+        );
+    }
 }
