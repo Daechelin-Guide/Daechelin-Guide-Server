eb5c84dc22d0507e0dbaf98543f38434057f1ed2
jiseok419 <pyosik0419@gmail.com>
2023-02-08 12:29:57 +0900

    :sparkles: :: 댓글 Entity

--- src/main/java/com/hackathon/daechelinguide/domain/comment/Comment.java
+++ src/main/java/com/hackathon/daechelinguide/domain/comment/Comment.java
@@ -1,10 +1,14 @@
 package com.hackathon.daechelinguide.domain.comment;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import lombok.*;
+import org.springframework.data.annotation.CreatedDate;
+
+import java.time.LocalDate;
 
 @Entity
 @Getter
@@ -16,4 +20,7 @@ public class Comment {
     private String message;
     private String date;
     private String menu;
+    @CreatedDate
+    @JsonFormat(pattern = "yyyy년 MM월 dd일 E요일")
+    private LocalDate createdDate;
 }
