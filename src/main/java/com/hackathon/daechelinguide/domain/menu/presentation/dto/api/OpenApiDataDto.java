2ce9ab9d9ac88c09e70e1911c216760c40eede1c
jiseok419 <pyosik0419@gmail.com>
2023-02-08 12:31:55 +0900

    :sparkles: :: 요일 추가

--- src/main/java/com/hackathon/daechelinguide/domain/menu/presentation/dto/api/OpenApiDataDto.java
+++ src/main/java/com/hackathon/daechelinguide/domain/menu/presentation/dto/api/OpenApiDataDto.java
@@ -4,16 +4,19 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.ToString;
 
 import java.io.Serializable;
 
 @Getter
+@ToString
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class OpenApiDataDto implements Serializable {
     @JsonProperty("data")
     private OpenApiInfoDto openApiInfoDto;
 
-    public OpenApiDataDto(OpenApiInfoDto openApiInfoDto) {
+    public OpenApiDataDto regeneration() {
         this.openApiInfoDto = new OpenApiInfoDto(openApiInfoDto);
+        return this;
     }
 }
--- src/main/java/com/hackathon/daechelinguide/domain/menu/presentation/dto/api/OpenApiInfoDto.java
+++ src/main/java/com/hackathon/daechelinguide/domain/menu/presentation/dto/api/OpenApiInfoDto.java
@@ -4,26 +4,40 @@ import com.hackathon.daechelinguide.domain.menu.Menu;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.ToString;
 
+import java.text.ParseException;
 import java.text.SimpleDateFormat;
-import java.util.Locale;
+import java.time.LocalDate;
+import java.util.Date;
 
 @Getter
+@ToString
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class OpenApiInfoDto {
     private String date;
     private String breakfast;
-    private String dinner;
+
     private String lunch;
+    private String dinner;
+
     private String week;
 
-
     public OpenApiInfoDto(OpenApiInfoDto data) {
+        final SimpleDateFormat parseFormat = new SimpleDateFormat("yyyy-MM-dd");
         this.date = data.getDate();
-        this.week = data.getWeek();
         this.breakfast = data.getBreakfast();
         this.dinner = data.getDinner();
         this.lunch = data.getLunch();
+        final Date parsedDate;
+        try {
+            parsedDate = parseFormat.parse(date);
+        } catch (ParseException e) {
+            throw new RuntimeException(e);
+        }
+
+        final SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy년 MM월 dd일 E요일");
+        this.week = dateFormat.format(parsedDate);
     }
 
     public static Menu toEntity(OpenApiInfoDto data) {
@@ -32,7 +46,7 @@ public class OpenApiInfoDto {
                 .breakfast(data.getBreakfast())
                 .dinner(data.getDinner())
                 .lunch(data.getLunch())
-                .week(data.getWeek())
+                .week(LocalDate.parse(data.getDate()))
                 .build();
     }
 }
